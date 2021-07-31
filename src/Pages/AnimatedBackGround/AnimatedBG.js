d6ba7e01d604237389eae4b63ce6d0996637c5ce
Jesse <jessemckinney513@gmail.com>
2021-07-31 14:05:21 -0400

    Anime.JS does not work well with style.module.css. I had to drop it and got with regular css

--- src/Pages/AnimatedBackGround/AnimatedBG.js
+++ src/Pages/AnimatedBackGround/AnimatedBG.js
@@ -1,23 +1,15 @@
 import React, { Component } from "react";
 // import anime from "animejs";
-import Styles from "./AnimatedBG.module.css";
+import "./AnimatedBG.css";
 
 class AnimatedBG extends Component {
   state = {};
 
   componentDidMount() {
-    // WORKING!!!
-    // let container = document.querySelector(".container");
-    // for (var i = 0; i <= 5; i++) {
-    //   let squares = document.createElement("div");
-    //   squares.classList.add("AnimatedBG_square__XWhz_");
-    //   container.appendChild(squares);
-    // }
-    // EXPERIMENTAL SECTION!!!
     let container = document.querySelector(".container");
     for (var i = 0; i <= 5; i++) {
       let squares = document.createElement("div");
-      squares.classList.add("AnimatedBG_square__XWhz_");
+      squares.classList.add("square");
       container.appendChild(squares);
     }
   }
@@ -25,14 +17,13 @@ class AnimatedBG extends Component {
   render() {
     return (
       <>
-        <div className={Styles.wrapper}>
-          <div id={Styles.container} className="container">
+        <div className="wrapper">
+          <div className="container">
             <h2>
               <span>My First video on</span>
               <br />
               Anime.JS
             </h2>
-            {/* <div className={Styles.square}></div> */}
           </div>
         </div>
       </>

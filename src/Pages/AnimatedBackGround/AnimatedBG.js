89fc3fd395ea0350e6e0b8ae5d3718a1b88409ac
Jesse <jessemckinney513@gmail.com>
2021-07-31 13:03:40 -0400

    yay

--- src/Pages/AnimatedBackGround/AnimatedBG.js
+++ src/Pages/AnimatedBackGround/AnimatedBG.js
@@ -5,11 +5,28 @@ import Styles from "./AnimatedBG.module.css";
 class AnimatedBG extends Component {
   state = {};
 
+  componentDidMount() {
+    // WORKING!!!
+    let container = document.querySelector(".container");
+    for (var i = 0; i <= 5; i++) {
+      let squares = document.createElement("div");
+      squares.classList.add("square");
+      container.appendChild(squares);
+    }
+    // EXPERIMENTAL SECTION!!!
+    // let container = document.querySelector(".container");
+    // for (var i = 0; i <= 5; i++) {
+    //   let div = document.createElement("div");
+    //   div.classList.add("box");
+    //   container.appendChild(div);
+    // }
+  }
+
   render() {
     return (
       <>
         <div className={Styles.wrapper}>
-          <div className={Styles.container}>
+          <div id={Styles.container} className="container">
             <h2>
               <span>My First video on</span>
               <br />

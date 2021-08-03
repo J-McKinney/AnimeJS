58ab48868bb86cc9789d156ddca591e752656f38
Jesse <jessemckinney513@gmail.com>
2021-08-03 16:10:09 -0400

    yay finally got some stuff working and looking smooth

--- src/Components/Card/HoverCard.js
+++ src/Components/Card/HoverCard.js
@@ -3,24 +3,22 @@ import Styles from "./HoverCard.module.css";
 
 function HoverCard(props) {
   return (
-    <>
-      <div className={Styles.card}>
-        <div className={Styles.imgBx} data-text={props.dataText}>
-          <img
-            className={Styles.hoverCardImg}
-            src={props.imgSrc}
-            alt={props.imgAlt}
-          />
-        </div>
-        <div className={Styles.content}>
-          <div>
-            <h3>{props.hoverCardTitle}</h3>
-            <p>{props.hoverCardParagraph}</p>
-            {/* <a href={props.link}>{props.linkTitle}</a> */}
-          </div>
+    <div className={Styles.card}>
+      <div className={Styles.imgBx} data-text={props.dataText}>
+        <img
+          className={Styles.hoverCardImg}
+          src={props.imgSrc}
+          alt={props.imgAlt}
+        />
+      </div>
+      <div className={Styles.content}>
+        <div>
+          <h3>{props.hoverCardTitle}</h3>
+          <p>{props.hoverCardParagraph}</p>
+          <a href={props.link}>{props.linkTitle}</a>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 

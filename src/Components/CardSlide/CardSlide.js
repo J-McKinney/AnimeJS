30e405caba80029c4db1a4aebf4c3dcfaee578c5
Jesse <jessemckinney513@gmail.com>
2021-08-06 16:19:03 -0400

    yay got props working and the images are showinggit add .!

--- src/Components/CardSlide/CardSlide.js
+++ src/Components/CardSlide/CardSlide.js
@@ -4,7 +4,15 @@ import Styles from "./CardSlide.module.css";
 function CardSlide(props) {
   return (
     <>
-      <div className={Styles.card}></div>
+      <div className={Styles.card}>
+        <div className={Styles.imgBx}>
+          <img src={props.img} alt={props.imgAlt} />
+        </div>
+        <div className={Styles.content}>
+          <h2 className={Styles.h2}>{props.cardTitle}</h2>
+          <p className={Styles.p}>{props.cardP}</p>
+        </div>
+      </div>
     </>
   );
 }
--- src/Pages/CardSlideFX/CardSlideFX.js
+++ src/Pages/CardSlideFX/CardSlideFX.js
@@ -1,11 +1,34 @@
 import React from "react";
+import CardSlide from "../../Components/CardSlide/CardSlide";
+import Code from "../../images/code.jpg";
+import Design from "../../images/design.jpg";
+import Launch from "../../images/launch.jpg";
 import Styles from "./CardSlideFX.module.css";
 
 const CardSlideFX = () => {
   return (
     <>
       <div className={Styles.wrapper}>
-        <div className={Styles.container}></div>
+        <div className={Styles.container}>
+          <CardSlide
+            img={Code}
+            imgAlt="Code Image"
+            cardTitle="Card One"
+            cardP="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum."
+          />
+          <CardSlide
+            img={Design}
+            imgAlt="Design Image"
+            cardTitle="Card Two"
+            cardP="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum."
+          />
+          <CardSlide
+            img={Launch}
+            imgAlt="Launch Image"
+            cardTitle="Card Three"
+            cardP="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum."
+          />
+        </div>
       </div>
     </>
   );
